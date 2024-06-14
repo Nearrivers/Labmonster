5a0690c21b9c4952978a772506a72917d8ac723b
Antoine <a.fourcade65@gmail.com>
2024-06-14 11:44:20 +0200

    bootstrap du projet

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+    <main>
+        <div id="result" class="result">{{ data.resultText }}</div>
+        <div id="input" class="input-box">
+            <input
+                id="name"
+                v-model="data.name"
+                autocomplete="off"
+                class="input"
+                type="text"
+            />
+            <Button class="btn" @click="greet">Button</Button>
+        </div>
+    </main>
+</template>
+
+<script lang="ts" setup>
+    import {reactive} from "vue"
+    import {Greet} from "../../wailsjs/go/main/App"
+    import {Button} from "@/components/ui/button"
+
+    const data = reactive({
+        name: "",
+        resultText: "Please enter your name below test 👇",
+    })
+
+    function greet() {
+        Greet(data.name).then((result) => {
+            data.resultText = result
+        })
+    }
+</script>
