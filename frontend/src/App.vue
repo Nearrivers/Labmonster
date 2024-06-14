dafb4e730d6665960168060337acaa14dceb4534
Antoine <a.fourcade65@gmail.com>
2024-06-14 18:07:14 +0200

    feat: création dialog de choix de répertoire + mise au point config vuejs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,74 @@
 <template>
-  <RouterView />
-  <!-- <HelloWorld />
-  <Button @click="QuitApp">Quitter l'application</Button> -->
+  <RouterView></RouterView>
+  <AlertDialog :open="isDialogOpen">
+    <AlertDialogContent>
+      <AlertDialogHeader class="!text-center">
+        <AlertDialogTitle>LabMonster</AlertDialogTitle>
+        <AlertDialogDescription>Version 0.0.1</AlertDialogDescription>
+      </AlertDialogHeader>
+      <div class="grid gap-4 py-4" v-if="!dir">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <div class="col-span-3">
+            <p class="leading-7 [&:not(:first-child)]:mt-6">
+              Créer un nouveau Lab
+            </p>
+            <p class="text-sm text-muted-foreground">
+              Crée un nouveau "Lab" dans un dossier
+            </p>
+          </div>
+          <Button @click="getLabDirectory">Créer</Button>
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <div class="col-span-3">
+            <p class="leading-7 [&:not(:first-child)]:mt-6">
+              Ouvrir un dossier comme lab
+            </p>
+            <p class="text-sm text-muted-foreground">
+              Défini un dossier comme "Lab"
+            </p>
+          </div>
+          <Button variant="outline">Ouvrir</Button>
+        </div>
+      </div>
+      <div class="flex items-center gap-4 py-4" v-else>
+        <div class="col-span-3">
+          <p class="leading-7 [&:not(:first-child)]:mt-6">Emplacement</p>
+          <p class="text-sm">
+            <span class="text-muted-foreground">
+              Votre nouveau Lab sera placé dans le dossier
+            </span>
+            <span class="font-semibold">{{ dir }}</span>
+          </p>
+        </div>
+        <div class="col-span-1 flex justify-center">
+          <Button>Valider</Button>
+        </div>
+      </div>
+    </AlertDialogContent>
+  </AlertDialog>
 </template>
 
 <script lang="ts" setup>
 import { useColorMode } from '@vueuse/core';
-import HelloWorld from './components/HelloWorld.vue';
 import { Button } from '@/components/ui/button';
-import { QuitApp } from '../wailsjs/go/topmenu/TopMenu';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { ref } from 'vue';
+import { OpenCreateLabDialog } from '$/config/AppConfig';
+
+const isDialogOpen = ref(true);
+const dir = ref('');
+
+async function getLabDirectory() {
+  try {
+    dir.value = await OpenCreateLabDialog();
+  } catch (error) {}
+}
 
 const mode = useColorMode();
 </script>
